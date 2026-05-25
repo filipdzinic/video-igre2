b6a83bd9661b74016ef14420577185f904641d99
filipdzinic <filipdzinic7@gmail.com>
2026-05-25 12:28:18 +0200

    Update cruid.h

--- videoigre/videoigre/cruid.h
+++ videoigre/videoigre/cruid.h
@@ -3,15 +3,18 @@
 
 #define FILE_NAME "igre.txt"
 
-// UBACI ŽANR, CIJENU ITD. ZA SORTIRANJE!
 typedef struct {
-	int id;
-	char ime[50];
+    int id;
+    char name[50];
+    char genre[50];
+    int year;
+    int price;
 } Igra;
 
+int usporediPoId(const void* a, const void* b);
+int usporediPoGodini(const void* a, const void* b);
+int usporediPoCijeni(const void* a, const void* b);
 void stvoriIgru();
 void citajIgru();
-void updateIgru();
-void obrisiIgru();
 
 #endif
