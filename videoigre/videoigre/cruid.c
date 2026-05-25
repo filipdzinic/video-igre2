7dc99fb973066df7ef99294a6eccbcaf5bbcdc0a
filipdzinic <filipdzinic7@gmail.com>
2026-05-25 12:28:00 +0200

    Update cruid.c

--- videoigre/videoigre/cruid.c
+++ videoigre/videoigre/cruid.c
@@ -5,11 +5,32 @@
 #include <string.h>
 #include "cruid.h"
 
+Igra popisIgara[100];
+int brojIgara = 0;
+
+int usporediPoId(const void* a, const void* b) {
+    Igra* igraA = (Igra*)a;
+    Igra* igraB = (Igra*)b;
+    return (igraA->id - igraB->id);
+}
+
+int usporediPoGodini(const void* a, const void* b) {
+	Igra* igraA = (Igra*)a;
+	Igra* igraB = (Igra*)b;
+	return (igraA->year - igraB->year);
+}
+
+int usporediPoCijeni(const void* a, const void* b) {
+	Igra* igraA = (Igra*)a;
+	Igra* igraB = (Igra*)b;
+	return (igraA->price - igraB->price);
+}
+
 void stvoriIgru() {
 	FILE* file = fopen(FILE_NAME, "a");
 
 	if (file == NULL) {
-		printf("\nERROR U DODAVANJU IGRE: Greska otvaranje datoteke\n");
+		printf("\nERROR U DODAVANJU IGRE: Areska otvaranje datoteke\n");
 		return;
 	}
 
@@ -18,23 +39,31 @@ void stvoriIgru() {
 	printf("\nUnesite ID igre: ");
 	scanf("%d", &k.id);
 
-	getchar();
+	getchar(); // Čisti \n iz međuspremnika
 
-	printf("\nUnesite ime igre: ");
-	fgets(k.ime, sizeof(k.ime), stdin);
+	printf("\nUnesite ime igre, poželjno bez razmaka: ");
+	fgets(k.name, sizeof(k.name), stdin);
+	k.name[strcspn(k.name, "\n")] = 0;
 
-	k.ime[strcspn(k.ime, "\n")] = 0;
+	printf("\nUnesite zanr igre: ");
+	fgets(k.genre, sizeof(k.genre), stdin);
+	k.genre[strcspn(k.genre, "\n")] = 0;
 
-	fprintf(file, "%d %s\n", k.id, k.ime);
-	printf(file, "%d %s dodano u datoteku.\n", k.id, k.ime);
+	printf("\nUnesite godinu izdanja igre: ");
+	scanf("%d", &k.year);
 
+	printf("\nUnesite cijenu igre: ");
+	scanf("%d", &k.price);
+
+	// Zapis u datoteku
+	fprintf(file, "%d %s %s %d %d\n", k.id, k.name, k.genre, k.year, k.price);
 	fclose(file);
 
-	printf("Igra uspjesno dodana\n");
+	// Ispis potvrde na ekran (ispravljen printf!)
+	printf("\nIgra %s uspjesno dodana u datoteku.\n", k.name);
 }
 
 void citajIgru() {
-
 	FILE* file = fopen(FILE_NAME, "r");
 
 	if (file == NULL) {
@@ -42,15 +71,58 @@ void citajIgru() {
 		return;
 	}
 
-	Igra k;
+	Igra popisIgara[100];
+	int brojIgara = 0;
 
-	printf("\n========== Popis igara ==========\n");
-
-	while (fscanf(file, "%d %[^\n]\n)", &k.id, k.ime) != EOF) {
-		printf("ID: %d Ime: %s\n", k.id, k.ime);
+	while (fscanf(file, "%d %s %s %d %d\n",
+		&popisIgara[brojIgara].id,
+		popisIgara[brojIgara].name,
+		popisIgara[brojIgara].genre,
+		&popisIgara[brojIgara].year,
+		&popisIgara[brojIgara].price) != EOF) {
+		brojIgara++;
+		if (brojIgara >= 100) break;
 	}
 
 	fclose(file);
+
+	if (brojIgara == 0) {
+		printf("\nDatoteka je prazna.\n");
+		return;
+	}
+
+	int odabir;
+	printf("\nKako želite sortirati popis?\n");
+	printf("1. Po ID-u\n");
+	printf("2. Po godini izdanja\n");
+	printf("3. Po cijeni\n");
+	printf("Odabir: ");
+	scanf("%d", &odabir);
+
+	switch (odabir) {
+	case 1:
+		qsort(popisIgara, brojIgara, sizeof(Igra), usporediPoId);
+		break;
+	case 2:
+		qsort(popisIgara, brojIgara, sizeof(Igra), usporediPoGodini);
+		break;
+	case 3:
+		qsort(popisIgara, brojIgara, sizeof(Igra), usporediPoCijeni);
+		break;
+	default:
+		printf("\nNeispravan odabir. Popis će biti prikazan nesortiran.\n");
+		break;
+	}
+
+	printf("\n\n========== Popis igara ==========\n");
+	for (int i = 0; i < brojIgara; i++) {
+		printf("ID: %d Ime: %s Žanr: %s Godina: %d Cijena: %d\n",
+			popisIgara[i].id,
+			popisIgara[i].name,
+			popisIgara[i].genre,
+			popisIgara[i].year,
+			popisIgara[i].price);
+	}
 }
 
 void updateIgru() {
@@ -82,10 +154,10 @@ void updateIgru() {
 		int trenutniID;
 		char trenutnoIme[100];
 
-		// Čitamo format s RAZMAKOM (%d %s) ili %d %[^\n] ako ima razmaka u imenu
+		// cita se format s razmakom (%d %s) ili %d %[^\n] ako ima razmaka u imenu
 		if (sscanf(linija, "%d %[^\n]", &trenutniID, trenutnoIme) >= 1) {
 
-			// Makni skriveni \r ili \n s kraja imena ako je zaostao iz fgets-a
+			// makni skriveni \r ili \n s kraja imena ako je ostao iz fgets-a
 			trenutnoIme[strcspn(trenutnoIme, "\r\n")] = 0;
 
 			if (trenutniID == trazeniID) {
@@ -99,7 +171,7 @@ void updateIgru() {
 				printf("\nIgra azurirana\n");
 			}
 
-			// KLJUČNO: Eksplicitno dodajemo \n na kraj svake linije da se ne spoje!
+			// dodajemo \n na kraj svake linije da se ne spoje
 			fprintf(temp, "%d %s\n", trenutniID, trenutnoIme);
 		}
 	}
@@ -128,7 +200,7 @@ void updateIgru() {
 
 	int trazeniID;
 	int pronaden = 0;
-	
+
 	Igra k;
 
 	printf("\nUnesite ID igre za brisanje: \n");
@@ -142,7 +214,7 @@ void updateIgru() {
 		}
 
 		fprintf(temp, "%d %s\n", k.id, k.ime);
-	} 
+	}
 
 	fclose(file);
 	fclose(temp);
@@ -189,9 +261,12 @@ void obrisiIgru() {
 	while (fgets(linija, sizeof(linija), file) != NULL) {
 		int trenutniID;
 		char trenutnoIme[100];
+		char trenutniGenre[100];
+		char trenutniYear[100];
+		char trenutniPrice[100];
 
 		// sscanf izvlači podatke iz pročitane linije
-		if (sscanf(linija, "%d %s", &trenutniID, trenutnoIme) == 2) {
+		if (sscanf(linija, "%d %s", &trenutniID, trenutnoIme, trenutniGenre, trenutniYear, trenutniPrice) == 2) {
 			if (trenutniID == trazeniID) {
 				pronaden = 1;
 				continue; // Preskoči upisivanje ove linije u temp (brišemo je)
