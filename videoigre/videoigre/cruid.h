2cf47a2b6d45b44993aeba8a9e9677c6f4219056
filipdzinic <filipdzinic7@gmail.com>
2026-06-01 11:45:28 +0200

    Update cruid.h

--- videoigre/videoigre/cruid.h
+++ videoigre/videoigre/cruid.h
@@ -14,9 +14,11 @@ typedef struct {
 int usporediPoId(const void* a, const void* b);
 int usporediPoGodini(const void* a, const void* b);
 int usporediPoCijeni(const void* a, const void* b);
+
 void stvoriIgru();
 void citajIgru();
 
+// KONCEPT 8 extern, potrebno za "case izlaz" u main izborniku inace se nece prepoznat
 extern Igra* popisIgara;
 extern int brojIgara;
 
